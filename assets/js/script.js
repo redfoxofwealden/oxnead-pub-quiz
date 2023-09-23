94337c400009b336b90f51e884a6b48b5943c039
redfoxofwealden <cameron.southofengland@yahoo.co.uk>
2023-09-23 11:58:02 +0100

    add list of questions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,104 @@ const buttonAnswer04 = document.getElementById('answer-4');
 const buttonNext = document.getElementById('next');
 const buttonResetStart = document.getElementById('reset-start');
 
+const paramsScoreTimer = {
+    Score: 0,
+    Timer: 99
+}
+
+/**
+ * Questions
+ */
+const listOfQuestions = [
+    {
+        question: 'What is the capital city of Czech Republic?',
+        answer: 'Prague',
+        option1: 'Bratislava',
+        option2: 'Budapest',
+        option3: 'Minsk'
+    },
+
+    {
+        question: 'What is the chemical symbol of aluminium?',
+        answer: 'Al',
+        option1: 'At',
+        option2: 'As',
+        option3: 'Ar'
+    },
+
+    {
+        question: 'Which country won the FIFA world cup in 2006?',
+        answer: 'Italy',
+        option1: 'France',
+        option2: 'Portugal',
+        option3: 'Germany'
+    },
+
+    {
+        question: 'What is the capital city of Estonia?',
+        answer: 'Tallinm',
+        option1: 'Vilnius',
+        option2: 'Riga',
+        option3: 'Tartu'
+    },
+
+    {
+        question: 'What is the capital city of Slovenia?',
+        answer: 'Ljubljana',
+        option1: 'Zagreb',
+        option2: 'Sarajevo',
+        option3: 'Podgorica'
+    },
+
+    {
+        question: 'What is the chemical symbol of sodium?',
+        answer: 'Na',
+        option1: 'S',
+        option2: 'Si',
+        option3: 'Sr'
+    },
+
+    {
+        question: 'What is the chemical symbol of aluminium?',
+        answer: 'Al',
+        option1: 'At',
+        option2: 'As',
+        option3: 'Ar'
+    },
+
+    {
+        question: 'What is the chemical symbol of tungsten?',
+        answer: 'W',
+        option1: 'Ta',
+        option2: 'Te',
+        option3: 'Sr'
+    },
+
+    {
+        question: 'What is the alternative name for the chemical element tungsten?',
+        answer: 'wolfram',
+        option1: 'antimony',
+        option2: 'bismuth',
+        option3: 'bauxite'
+    },
+
+    {
+        question: 'Who was the director of the film Apollo 13?',
+        answer: 'Ron Howard',
+        option1: 'Steven Spielberg',
+        option2: 'James Cameron',
+        option3: 'Alan Parker'
+    },
+
+    {
+        question: 'In which country does Oktoberfest takes place?',
+        answer: 'Germany',
+        option1: 'Austria',
+        option2: 'Switzerland',
+        option3: 'Luxembourg'
+    }
+];
+
 document.addEventListener('DOMContentLoaded', function(event) {
 
     /*
